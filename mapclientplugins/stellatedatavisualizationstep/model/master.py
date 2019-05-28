e111a74bc2cbf25c75a40559137935c616af67a4
zekh167 <35438430+zekh167@users.noreply.github.com>
2019-05-28 16:16:27 +1200

    MasterModel constructed

---
+++ mapclientplugins/stellatedatavisualizationstep/model/master.py
@@ -0,0 +1,12 @@
+from opencmiss.zinc.context import Context
+from opencmiss.zinc.material import Material
+
+from opencmiss.utils.zinc import define_standard_visualisation_tools
+
+from .cube_model import CubeModel
+
+
+class MasterModel(object):
+
+    def __init__(self):
+        pass

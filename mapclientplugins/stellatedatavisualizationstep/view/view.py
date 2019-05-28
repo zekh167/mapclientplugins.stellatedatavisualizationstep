fbbefe590b1b208dcaa2ce3d8aaabfed58e39215
zekh167 <35438430+zekh167@users.noreply.github.com>
2019-05-28 17:03:49 +1200

    A View object to handle UI.

--- mapclientplugins/stellatedatavisualizationstep/view/view.py
+++ mapclientplugins/stellatedatavisualizationstep/view/view.py
@@ -1,227 +1,51 @@
-# -*- coding: utf-8 -*-
+from PySide import QtGui, QtCore
 
-# Form implementation generated from reading ui file '..\..\..\sparc\codes\mapclient-plugins\mapclientplugins.stellatedatavisualizationstep\mapclientplugins\stellatedatavisualizationstep\qt\view.ui'
-#
-# Created: Tue May 28 15:38:14 2019
-#      by: pyside-uic 0.2.15 running on PySide 1.2.4
-#
-# WARNING! All changes made in this file will be lost!
+from .ui_view import Ui_StellateVisualizationWidget
 
-from PySide import QtCore, QtGui
+from opencmiss.zinchandlers.scenemanipulation import SceneManipulation
 
-class Ui_StellateVisualizationWidget(object):
-    def setupUi(self, StellateVisualizationWidget):
-        StellateVisualizationWidget.setObjectName("StellateVisualizationWidget")
-        StellateVisualizationWidget.resize(1155, 736)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(StellateVisualizationWidget.sizePolicy().hasHeightForWidth())
-        StellateVisualizationWidget.setSizePolicy(sizePolicy)
-        self.horizontalLayout = QtGui.QHBoxLayout(StellateVisualizationWidget)
-        self.horizontalLayout.setSpacing(0)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.dockWidget = QtGui.QDockWidget(StellateVisualizationWidget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.dockWidget.sizePolicy().hasHeightForWidth())
-        self.dockWidget.setSizePolicy(sizePolicy)
-        self.dockWidget.setMinimumSize(QtCore.QSize(346, 135))
-        self.dockWidget.setFeatures(QtGui.QDockWidget.DockWidgetFloatable|QtGui.QDockWidget.DockWidgetMovable)
-        self.dockWidget.setAllowedAreas(QtCore.Qt.AllDockWidgetAreas)
-        self.dockWidget.setObjectName("dockWidget")
-        self.dockWidgetContents = QtGui.QWidget()
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.dockWidgetContents.sizePolicy().hasHeightForWidth())
-        self.dockWidgetContents.setSizePolicy(sizePolicy)
-        self.dockWidgetContents.setObjectName("dockWidgetContents")
-        self.verticalLayout_2 = QtGui.QVBoxLayout(self.dockWidgetContents)
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.scrollArea = QtGui.QScrollArea(self.dockWidgetContents)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.scrollArea.sizePolicy().hasHeightForWidth())
-        self.scrollArea.setSizePolicy(sizePolicy)
-        self.scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setObjectName("scrollArea")
-        self.scrollAreaWidgetContents = QtGui.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 344, 712))
-        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.verticalLayout_3 = QtGui.QVBoxLayout(self.scrollAreaWidgetContents)
-        self.verticalLayout_3.setSpacing(2)
-        self.verticalLayout_3.setContentsMargins(2, 2, 2, 2)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.toolBox = QtGui.QToolBox(self.scrollAreaWidgetContents)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.toolBox.sizePolicy().hasHeightForWidth())
-        self.toolBox.setSizePolicy(sizePolicy)
-        self.toolBox.setStyleSheet("QToolBox::tab {\n"
-"         background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
-"                                     stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,\n"
-"                                     stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);\n"
-"         border-radius: 5px;\n"
-"         color: black;\n"
-"     }\n"
-"\n"
-"     QToolBox::tab:selected { /* italicize selected tabs */\n"
-"         font: bold;\n"
-"         color: black;\n"
-"     }\n"
-"QToolBox {\n"
-"    padding : 0\n"
-"}")
-        self.toolBox.setObjectName("toolBox")
-        self.cellPage = QtGui.QWidget()
-        self.cellPage.setGeometry(QtCore.QRect(0, 0, 340, 652))
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.cellPage.sizePolicy().hasHeightForWidth())
-        self.cellPage.setSizePolicy(sizePolicy)
-        self.cellPage.setObjectName("cellPage")
-        self.verticalLayout_5 = QtGui.QVBoxLayout(self.cellPage)
-        self.verticalLayout_5.setSpacing(7)
-        self.verticalLayout_5.setContentsMargins(0, 3, 0, 3)
-        self.verticalLayout_5.setObjectName("verticalLayout_5")
-        self.ViewGroupBox = QtGui.QGroupBox(self.cellPage)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.ViewGroupBox.sizePolicy().hasHeightForWidth())
-        self.ViewGroupBox.setSizePolicy(sizePolicy)
-        self.ViewGroupBox.setMaximumSize(QtCore.QSize(16777215, 16777215))
-        font = QtGui.QFont()
-        font.setPointSize(8)
-        font.setWeight(75)
-        font.setBold(True)
-        self.ViewGroupBox.setFont(font)
-        self.ViewGroupBox.setCheckable(False)
-        self.ViewGroupBox.setObjectName("ViewGroupBox")
-        self.verticalLayout_7 = QtGui.QVBoxLayout(self.ViewGroupBox)
-        self.verticalLayout_7.setSpacing(3)
-        self.verticalLayout_7.setContentsMargins(3, 3, 3, 3)
-        self.verticalLayout_7.setObjectName("verticalLayout_7")
-        self.radioButton = QtGui.QRadioButton(self.ViewGroupBox)
-        self.radioButton.setObjectName("radioButton")
-        self.verticalLayout_7.addWidget(self.radioButton)
-        self.radioButton_2 = QtGui.QRadioButton(self.ViewGroupBox)
-        self.radioButton_2.setObjectName("radioButton_2")
-        self.verticalLayout_7.addWidget(self.radioButton_2)
-        self.viewerWidgets = QtGui.QWidget(self.ViewGroupBox)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.viewerWidgets.sizePolicy().hasHeightForWidth())
-        self.viewerWidgets.setSizePolicy(sizePolicy)
-        self.viewerWidgets.setObjectName("viewerWidgets")
-        self.gridLayout = QtGui.QGridLayout(self.viewerWidgets)
-        self.gridLayout.setSpacing(3)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setObjectName("gridLayout")
-        self.verticalLayout_7.addWidget(self.viewerWidgets)
-        self.radioButton_3 = QtGui.QRadioButton(self.ViewGroupBox)
-        self.radioButton_3.setObjectName("radioButton_3")
-        self.verticalLayout_7.addWidget(self.radioButton_3)
-        self.radioButton_4 = QtGui.QRadioButton(self.ViewGroupBox)
-        self.radioButton_4.setObjectName("radioButton_4")
-        self.verticalLayout_7.addWidget(self.radioButton_4)
-        self.radioButton_5 = QtGui.QRadioButton(self.ViewGroupBox)
-        self.radioButton_5.setObjectName("radioButton_5")
-        self.verticalLayout_7.addWidget(self.radioButton_5)
-        self.verticalLayout_5.addWidget(self.ViewGroupBox)
-        self.registerGroupBox = QtGui.QGroupBox(self.cellPage)
-        font = QtGui.QFont()
-        font.setPointSize(8)
-        font.setWeight(75)
-        font.setBold(True)
-        self.registerGroupBox.setFont(font)
-        self.registerGroupBox.setCheckable(False)
-        self.registerGroupBox.setObjectName("registerGroupBox")
-        self.verticalLayout_8 = QtGui.QVBoxLayout(self.registerGroupBox)
-        self.verticalLayout_8.setSpacing(3)
-        self.verticalLayout_8.setContentsMargins(3, 3, 3, 3)
-        self.verticalLayout_8.setObjectName("verticalLayout_8")
-        self.viewerWidgets_2 = QtGui.QWidget(self.registerGroupBox)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.viewerWidgets_2.sizePolicy().hasHeightForWidth())
-        self.viewerWidgets_2.setSizePolicy(sizePolicy)
-        self.viewerWidgets_2.setObjectName("viewerWidgets_2")
-        self.gridLayout_5 = QtGui.QGridLayout(self.viewerWidgets_2)
-        self.gridLayout_5.setSpacing(3)
-        self.gridLayout_5.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout_5.setObjectName("gridLayout_5")
-        self.verticalLayout_8.addWidget(self.viewerWidgets_2)
-        self.registerpushButton = QtGui.QPushButton(self.registerGroupBox)
-        self.registerpushButton.setObjectName("registerpushButton")
-        self.verticalLayout_8.addWidget(self.registerpushButton)
-        self.verticalLayout_5.addWidget(self.registerGroupBox)
-        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.verticalLayout_5.addItem(spacerItem)
-        self.toolBox.addItem(self.cellPage, "")
-        self.verticalLayout_3.addWidget(self.toolBox)
-        self.frame = QtGui.QFrame(self.scrollAreaWidgetContents)
-        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName("frame")
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.frame)
-        self.horizontalLayout_2.setContentsMargins(3, 3, 3, 3)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.viewAllButton = QtGui.QPushButton(self.frame)
-        self.viewAllButton.setObjectName("viewAllButton")
-        self.horizontalLayout_2.addWidget(self.viewAllButton)
-        self.doneButton = QtGui.QPushButton(self.frame)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.doneButton.sizePolicy().hasHeightForWidth())
-        self.doneButton.setSizePolicy(sizePolicy)
-        self.doneButton.setObjectName("doneButton")
-        self.horizontalLayout_2.addWidget(self.doneButton)
-        self.verticalLayout_3.addWidget(self.frame)
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.verticalLayout_2.addWidget(self.scrollArea)
-        self.dockWidget.setWidget(self.dockWidgetContents)
-        self.horizontalLayout.addWidget(self.dockWidget)
-        self.sceneviewerWidget = BaseSceneviewerWidget(StellateVisualizationWidget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(1)
-        sizePolicy.setVerticalStretch(6)
-        sizePolicy.setHeightForWidth(self.sceneviewerWidget.sizePolicy().hasHeightForWidth())
-        self.sceneviewerWidget.setSizePolicy(sizePolicy)
-        self.sceneviewerWidget.setObjectName("sceneviewerWidget")
-        self.horizontalLayout.addWidget(self.sceneviewerWidget)
 
-        self.retranslateUi(StellateVisualizationWidget)
-        self.toolBox.setCurrentIndex(0)
-        self.toolBox.layout().setSpacing(2)
-        QtCore.QMetaObject.connectSlotsByName(StellateVisualizationWidget)
+class View(QtGui.QWidget):
 
-    def retranslateUi(self, StellateVisualizationWidget):
-        StellateVisualizationWidget.setWindowTitle(QtGui.QApplication.translate("StellateVisualizationWidget", "Form", None, QtGui.QApplication.UnicodeUTF8))
-        self.dockWidget.setWindowTitle(QtGui.QApplication.translate("StellateVisualizationWidget", "Stellate Ganglion Data Registration", None, QtGui.QApplication.UnicodeUTF8))
-        self.ViewGroupBox.setTitle(QtGui.QApplication.translate("StellateVisualizationWidget", "View", None, QtGui.QApplication.UnicodeUTF8))
-        self.radioButton.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "Sample Image 1", None, QtGui.QApplication.UnicodeUTF8))
-        self.radioButton_2.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "Sample Image 2", None, QtGui.QApplication.UnicodeUTF8))
-        self.radioButton_3.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "Sample Image 3", None, QtGui.QApplication.UnicodeUTF8))
-        self.radioButton_4.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "Sample Image 4", None, QtGui.QApplication.UnicodeUTF8))
-        self.radioButton_5.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "Sample Image 5", None, QtGui.QApplication.UnicodeUTF8))
-        self.registerGroupBox.setTitle(QtGui.QApplication.translate("StellateVisualizationWidget", "Data", None, QtGui.QApplication.UnicodeUTF8))
-        self.registerpushButton.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "Register the cell data", None, QtGui.QApplication.UnicodeUTF8))
-        self.toolBox.setItemText(self.toolBox.indexOf(self.cellPage), QtGui.QApplication.translate("StellateVisualizationWidget", "Control Panel", None, QtGui.QApplication.UnicodeUTF8))
-        self.viewAllButton.setToolTip(QtGui.QApplication.translate("StellateVisualizationWidget", "Adjust the view to see the whole model", None, QtGui.QApplication.UnicodeUTF8))
-        self.viewAllButton.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "View All", None, QtGui.QApplication.UnicodeUTF8))
-        self.doneButton.setToolTip(QtGui.QApplication.translate("StellateVisualizationWidget", "Finish this step", None, QtGui.QApplication.UnicodeUTF8))
-        self.doneButton.setText(QtGui.QApplication.translate("StellateVisualizationWidget", "Done", None, QtGui.QApplication.UnicodeUTF8))
+    def __init__(self, model, parent=None):
+        super(View, self).__init__(parent)
+        self._ui = Ui_StellateVisualizationWidget()
+        self._ui.setupUi(self)
 
-from opencmiss.zincwidgets.basesceneviewerwidget import BaseSceneviewerWidget
+        self._setup_handlers()
+
+        self._model = model
+        self._ui.sceneviewerWidget.set_context(self._model.get_context())
+
+        self._done_callback = None
+        self._settings = {'view-parameters': {}}
+
+        self._make_connections()
+
+    def _make_connections(self):
+        self._ui.sceneviewerWidget.graphics_initialized.connect(self._graphics_initialized)
+
+    def _graphics_initialized(self):
+        scene_viewer = self._ui.sceneviewerWidget.get_zinc_sceneviewer()
+        if scene_viewer is not None:
+            scene = self._model.get_scene()
+            self._ui.sceneviewerWidget.set_scene(scene)
+            if len(self._settings['view-parameters']) == 0:
+                self._view_all()
+            else:
+                eye = self._settings['view-parameters']['eye']
+                look_at = self._settings['view-parameters']['look_at']
+                up = self._settings['view-parameters']['up']
+                angle = self._settings['view-parameters']['angle']
+                self._ui.sceneviewerWidget.set_view_parameters(eye, look_at, up, angle)
+
+    def _view_all(self):
+        if self._ui.sceneviewerWidget.get_zinc_sceneviewer() is not None:
+            self._ui.sceneviewerWidget.view_all()
+
+    def register_done_callback(self, done_callback):
+        self._done_callback = done_callback
+
+    def _setup_handlers(self):
+        basic_handler = SceneManipulation()
+        self._ui.sceneviewerWidget.register_handler(basic_handler)
